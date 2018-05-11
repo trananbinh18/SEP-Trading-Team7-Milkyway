aac6ddf645c9611a6b7976d06894aeee84d5eff0
TranAnhPhuongbl1997 <trananhphuongbl112@gmail.com>
2018-05-11 09:57:13 +0700

    Validate view Addproduct

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -17,7 +17,35 @@ class Controller extends BaseController
     	return view('catalog_sidebar')->with('minh',$name);
     }
 
+
     function Postproduct(Request $Product_request){
+
+        $this->validate($Product_request,
+        [
+            'cbCategory=>'=>'required',
+            'tensanpham'=>'required|min:2|max:50',
+            'Soluong'=>'required',
+            'Giasanpham'=>'required',
+            'cbDonvi'=>'required',
+            'imagesSP'=>'required',
+            'imagesGCN'=>'required',
+            'mieutasanpham'=>'required|min:10|max:200'
+        ],
+        [
+            'cbCategory.required'=>'Bạn chưa chọn danh mục cho sản phẩm',
+            'tensanpham.required'=>'Bạn chưa đặt tên sản phẩm',
+            'tensanpham.min'=>'Tên sản phẩm phải từ 2 đến 50 ký tự',
+            'tensanpham.max'=>'Tên sản phẩm phải từ 2 đến 50 ký tự',
+            'Soluong.required'=>'Bạn chưa nhập số lượng',
+            'cbDonvi.required'=>'Bạn chưa chọn đơn vị',
+            'imagesSP.required'=>'Bạn chưa chọn ảnh sản phẩm',
+            'imagesGCN.required'=>'Bạn chưa chọn ảnh chứng nhận sản phẩm',
+            'mieutasanpham.required'=>'Bạn chưa miêu tả sản phẩm',
+            'mieutasanpham.min'=>'Miêu tả sản phẩm phải từ 10 đến 200 ký tự',
+            'mieutasanpham.max'=>'Miêu tả sản phẩm phải từ 10 đến 200 ký tự'
+        ]);
+           
+
             $filenameSP = $Product_request->file('imagesSP')->getclientOriginalName();
             $filenameGCN = $Product_request->file('imagesGCN')->getclientOriginalName();
             $Product = new sanpham();
@@ -34,6 +62,7 @@ class Controller extends BaseController
             $Product_request->file('imagesGCN')->move('resources\assets\images\Certificate',$filenameGCN);
             $Product->save();
 
+            return redirect('Addproduct')->with('thongbao','Bạn đã thêm thành công');
            
     }
     
