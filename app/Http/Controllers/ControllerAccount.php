367c1f2820d6636655326f5fff86ad8b2cd3d2a4
Wingcfly <wesleynguyen1411@gmail.com>
2018-05-13 00:30:27 +0700

    Update Login

--- app/Http/Controllers/ControllerAccount.php
+++ app/Http/Controllers/ControllerAccount.php
@@ -10,14 +10,10 @@ use App\Http\Controllers\Controller;
 class ControllerAccount extends Controller
 {
     function Login(Request $re){
-    	//Lấy dữ liệu từ DB và kiểm tra email nhập từ form có DB hay không, nếu có thì lưu vào $user
-    	$user = DB::table('nhanvien')->select('email')->where('email','=',$re->email)->get();
-    	//Kiểm tra $user, nếu có dữ liệu tức là email có tồn tại trong hệ thống, nếu k tức là email k có trong hệ thống
-    	if(!empty($user)){
-    		//Nếu email có tồn tại trong hệ thống thì Đăng nhập thành công và trả về view Home (định danh route: homepage)
-    		return redirect()->route('homepage');
-    	}
-    	//Nếu email không tồn tại thì trở về trang Đăng nhập
-    	return redirect()->back();
+    	$user = DB::table('nhanvien')->select('email')->where('email',$re->email)->get();
+        if(count($user) >= 1) {
+            return redirect()->route('homepage');
+        }
+        return redirect()->back();
     }
 }
