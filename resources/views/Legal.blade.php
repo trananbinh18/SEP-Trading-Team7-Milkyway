73fb6e2e229a068d646d58119356a11e2bc0a4aa
Hoa Tran <tranphuhoa1995@gmail.com>
2018-05-16 23:32:33 +0700

    Create legal

---
+++ resources/views/Legal.blade.php
@@ -0,0 +1,50 @@
+        @extends('layout.master')
+        @section (' title ')
+        Pháp lý
+        @stop
+        @section('content')
+        <div class="main-content">
+            <div class="container">
+                <div class="banner-header banner-lbook3">
+                    <img src="resources/assets/images/banner-product-checkout.jpg" alt="Banner-header">
+                    <div class="text">
+                    <h3>Pháp lý</h3>
+                    <p><a href="#" title="Home">Trang chủ</a><i class="fa fa-caret-right" ></i>Pháp lý</p>
+                </div>
+                </div>
+                <div class="page-faq">
+                <div class="container container-ver2">
+                    <div class="content-text space-50">
+                    	<br></br>
+                        <div class="row">
+                            <div class="col-md-6 space-50">
+                                <h3>Quy định về bảo mật</h3>
+                                <p>- Trang web của chúng tôi coi trọng việc bảo mật thông tin và sử dụng các biện pháp tốt nhất bảo vệ thông tin và việc thanh toán của quý khách. Thông tin của quý khách trong quá trình thanh toán sẽ được mã hóa để đảm bảo an toàn. Sau khi quý khách hoàn thành quá trình đặt hàng, quý khách sẽ thoát khỏi chế độ an toàn.</p>
+                            </div>
+                            <div class="col-md-6 space-50">
+                                <h3>Thương hiệu và bản quyền</h3>
+                                <p>- Mọi quyền sở hữu trí tuệ (đã đăng ký hoặc chưa đăng ký), nội dung thông tin và tất cả các thiết kế, văn bản, đồ họa, phần mềm, hình ảnh, video, âm nhạc, âm thanh, biên dịch phần mềm, mã nguồn và phần mềm cơ bản đều là tài sản của chúng tôi. Toàn bộ nội dung của trang web được bảo vệ bởi luật bản quyền của Việt Nam và các công ước quốc tế. Bản quyền đã được bảo lưu.</p>
+                            </div>
+                            <div class="col-md-6 space-50">
+                                <h3>Quyền pháp lý</h3>
+                                <p>- Các điều kiện, điều khoản và nội dung của trang web này được điều chỉnh bởi luật pháp Việt Nam và Tòa án có thẩm quyền tại Việt Nam sẽ giải quyết bất kỳ tranh chấp nào phát sinh từ việc sử dụng trái phép trang web này.</p>
+                            </div>
+                            <div class="col-md-6 space-50">
+                                <h3>Luật pháp và thẩm quyền tại Lãnh thổ Việt Nam</h3>
+                                <p>- Tất cả các Điều Khoản và Điều Kiện này và Hợp Đồng (và tất cả nghĩa vụ phát sinh ngoài hợp đồng hoặc có liên quan) sẽ bị chi phối và được hiểu theo luật pháp của Việt Nam. Nếu có tranh chấp phát sinh bởi các Quy định Sử dụng này, quý khách hàng có quyền gửi khiếu nại/khiếu kiện lên Tòa án có thẩm quyền tại Việt Nam để giải quyết.</p>
+                            </div>
+                            <div class="col-md-6 space-50">
+                                <h3>Đảm bảo an toàn giao dịch tại Whole Foods Market</h3>
+                                <p>- Chúng tôi sử dụng các dịch vụ để bảo vệ thông tin về nội dung mà người bán đăng  sản phẩm trên Whole Foods Market. Để đảm bảo các giao dịch được tiến hành thành công, hạn chế tối đa rủi ro có thể phát sinh.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+          </div>
+        <div id="back-to-top">
+            <i class="fa fa-long-arrow-up"></i>
+        </div>
+        @stop
+       
