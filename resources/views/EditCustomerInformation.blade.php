9966ff10187199ee944e17e826890a495d59b49c
Hoa Tran <tranphuhoa1995@gmail.com>
2018-05-19 14:22:02 +0700

    Update Information

--- resources/views/EditCustomerInformation.blade.php
+++ resources/views/EditCustomerInformation.blade.php
@@ -8,12 +8,12 @@
             <div>
               <aside class="widget widget_product_categories" style="padding-left: 10px;">
                       <div class="categories-home3">
-                         <h3>Thông tin khách hàng</h3>
+                         <h3>Xin chào Hòa</h3>
                           <i class="fa fa-chevron-circle-down icon-click"></i>
                           <ul class="menu-vertical">
                               <li><img src="resources/assets/images/icon-categories-1.png" alt=""><a href="#" title="Đơn hàng">Đơn hàng</a></li>
-                              <li><img src="resources/assets/images/icon-categories-8.png" alt=""><a href="{{route('editInf')}}" title="Sửa thông tin">Sửa thông tin</a></li>
-                              <li><img src="resources/assets/images/icon-categories-8.png" alt=""><a href="{{route('resetpass')}}"" title="Đổi mật khẩu">Đổi mật khẩu</a></li>
+                              <li><img src="resources/assets/images/icon-choose-2.png" alt=""><a href="{{route('editInf')}}" title="Sửa thông tin">Sửa thông tin</a></li>
+                              <li><img src="resources/assets/images/icon-choose-4.png" alt=""><a href="{{route('resetpass')}}"" title="Đổi mật khẩu">Đổi mật khẩu</a></li>
                           </ul>
                       </div>
                   </aside>
@@ -27,47 +27,47 @@
                   @endif
                 <form class="form-horizontal" action="{{route('changeInfor')}}" role="form" method="POST">
                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                  <div style="padding-left: 240px;">
-                    <h3 style="font-weight: 400; font-size: 18px; font-family: 'Roboto Slab'">THÔNG TIN TÀI KHOẢN</h3>
+                  <div class="center">
+                    <h2>THÔNG TIN TÀI KHOẢN</h2>
                     <br>
                   </div>
-                  <div class="form-group">
-                    <label style="font-weight: 400; font-size: 18px; font-family: 'Roboto Slab'" for="inputEmail3" class="col-sm-3 control-lab">Họ và tên*</label>
+                  <div class="form-group signupseller">
+                    <label for="inputEmail3" class="col-sm-3 control-lab">Họ và tên</label>
                     <div class="col-sm-9">
                       <input type="text" class="form-control" id="inputEmail3" placeholder="Họ và tên" name="name" required>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <label style="font-weight: 400; font-size: 18px; font-family: 'Roboto Slab'" for="inputEmail3" class="col-sm-3 control-lab">Số điện thoại*</label>
+                  <div class="form-group signupseller">
+                    <label for="inputpass" class="col-sm-3 control-lab">Số điện thoại</label>
                     <div class="col-sm-9">
                       <input type="text" class="form-control" id="inputEmail3" placeholder="Số điện thoại" name="phone" value="{!! old('name', isset($data)) ? $data['SĐT']  : null !!}" required >
                     </div>
                   </div>
-                  <div class="form-group">
-                    <label style="font-weight: 400; font-size: 18px; font-family: 'Roboto Slab'" for="inputEmail3" class="col-sm-3 control-lab">Số nhà*</label>
+                  <div class="form-group signupseller">
+                    <label for="inputpass" class="col-sm-3 control-lab">Địa chỉ</label>
                     <div class="col-sm-9">
-                      <input type="text" class="form-control" id="inputEmail3" placeholder="Số nhà" name="number_house" value="{!! old('name', isset($data)) ? $data['SONHA']  : null !!}" required>
+                      <input type="text" class="form-control" id="inputEmail3" placeholder="Địa chỉ" name="number_house" value="{!! old('name', isset($data)) ? $data['SONHA']  : null !!}" required>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <label style="font-weight: 400; font-size: 18px; font-family: 'Roboto Slab'" for="inputEmail3" class="col-sm-3 control-lab">Phường*</label>
+                  <div class="form-group signupseller">
+                    <label for="inputpass" class="col-sm-3 control-lab">Phường</label>
                     <div class="col-sm-9">
                       <input type="text" class="form-control" id="inputEmail3" placeholder="Phường" name="ward" value="{!! old('name', isset($data)) ? $data['PHUONG']  : null !!}" required>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <label style="font-weight: 400; font-size: 18px; font-family: 'Roboto Slab'" for="inputEmail3" class="col-sm-3 control-lab">Quận*</label>
+                  <div class="form-group signupseller">
+                    <label for="inputpass" class="col-sm-3 control-lab">Quận</label>
                     <div class="col-sm-9">
                       <input type="text" class="form-control" id="inputEmail3" placeholder="Quận" name="district" value="{!! old('name', isset($data)) ? $data['QUAN']  : null !!}" required>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <label style="font-weight: 400; font-size: 18px; font-family: 'Roboto Slab'" for="inputEmail3" class="col-sm-3 control-lab">Thành phố*</label>
+                  <div class="form-group signupseller">
+                    <label for="inputpass" class="col-sm-3 control-lab">Thành phố</label>
                     <div class="col-sm-9">
                       <input type="text" class="form-control" id="inputEmail3" placeholder="Thành phố" name="city" value="{!! old('name', isset($data)) ? $data['TP']  : null !!}" required>
                     </div>
                   </div>
-                  <div style="padding-left: 240px;">
+                  <div class="col-md-12 center">
                     <br>
                     <a href="#"><button type="submit" class="link-v1 rt">Cập nhật</button></a>
                   </div>
