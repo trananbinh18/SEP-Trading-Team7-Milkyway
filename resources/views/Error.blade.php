941c9502718c57526c5a093bda5b918fc56c8695
Bach tran <Bachtran9497@gmail.com>
2018-05-24 06:56:12 +0700

    Add View Error

---
+++ resources/views/Error.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.master')
+@section (' title ')
+Error
+@stop
+@section('content')
+	<div class="text center">
+        <div class="icon-box box">
+            <img src="resources/assets/images/icon-page404.png" alt="icon">
+        </div>
+        <div class="text texterror">
+        	<p>Trang này không tồn tại !</p>
+        </div>
+        <div class="buttonerror">
+            <a href="#"><button type="submit" class="link-v1 rt">Quay lại trang chủ</button></a>
+        </div>
+    </div>
+@stop
