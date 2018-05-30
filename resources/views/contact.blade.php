5cba74a66b821c34e885494cacf828e5b28c55be
Bach tran <Bachtran9497@gmail.com>
2018-05-30 11:58:33 +0700

    Update view Contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,8 @@
         @stop
         @section('content')
         <div class="container">
-            <div id="googleMap">
+            <div id="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d582.6568948683289!2d106.69326499064714!3d10.762760697253603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x949d258c9508b1f2!2zxJDhuqFpIGjhu41jIFbEg24gTGFuZyBjxqEgc-G7nyAx!5e0!3m2!1svi!2s!4v1527655200556" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
         </div>
         <!-- End google map -->
