5e97ab6583f2f2f13ac1c618c5d88adb4bf72d4a
Bach tran <Bachtran9497@gmail.com>
2018-05-30 11:56:02 +0700

    Update view changStatus

--- resources/views/ChangeStatus.blade.php
+++ resources/views/ChangeStatus.blade.php
@@ -1,45 +1,60 @@
- <!DOCTYPE html>
- 
-@extends('layout.master')
- @section('content')  
-       <div>
-         <div class="row Ttttk">
-            <div class="col-md-3 LTtttk">
-                <p> </p>
-                <h3>Trạng Thái</h3>
-            </div>
-            <div class="col-md-9 RTtttk">       
-                <h2>Danh sách sản phẩm</h2>
-            </div>
-         </div>
-         <br>
-         <div class="row Btttk">
-           <div class="col-md-3 LBtttk">
-             <p><a style="color: black" href="" >Thay đổi trạng thái sản phẩm</a></p>
+    @extends('layout.master')
+    @section (' title ')
+    Trạng thái
+    @stop
+    @section('content')
+    <div class="row row-addproduct">
+      <div class="col-md-3 box-shadow control-infotproduct">
+        <div>
+          <aside class="widget widget_product_categories" style="padding-left: 10px;">
+            <div class="categories-home3">
+             <h3>TRẠNG THÁI</h3>
+             <i class="fa fa-chevron-circle-down icon-click"></i>
+             <ul class="menu-vertical">
+               <li><img src="resources/assets/images/icon-choose-5.png" alt=""><a href="">Trạng thái sản phẩm</a></li>
+             </ul>
            </div>
-           <form class="form-horizontal" action="{{route('ChangeStatus')}}" role="form" method="POST">
-  
-           <div class="col-md-5 ">
-              <div class="form-group">
-                 @if(session('thongbao')) 
-              <div class="alert-success">
-                {{session('thongbao')}}
+         </aside>
+       </div>
+     </div>
+     <div class="col-md-8 control-infotproduct box-shadow">
+      @if(session('thongbao'))
+      <div class="alert-success">
+        {{session('thongbao')}}
+      </div>
+      @endif
+      <div class="page">
+        <div class="container container-ver2">
+          <div class="content-text space-50">
+
+            <form class="form-horizontal" role="form" action="{{route('ChangeStatus')}}" method="POST">
+             <div class="col-md-8 space-80" id="bodyinput">
+              <div class="center">
+                <h2>DUYỆT SẢN PHẨM</h2>
               </div>
-                 @endif
+              <div class="form-group signupseller">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                 <input type="hidden" name="MASP" value="<?php echo "{$status['MASP']}";?>">
-                <label class="control-label" for="full_name">Trạng thái</label>
-                <div class="input-wrap">
+                <input type="hidden" name="MASP" value="<?php echo "{$status['MASP']}";?>">
+                <label for="" class="col-md-3 control-lab">Trạng thái</label>
+                <div class="col-md-2">
                   <input type="text" class="form-control" value="<?php echo "{$status['TRANGTHAI']}"; ?>" placeholder="trạng thái" name="status">
-                  <span class="help-block"></span>
                 </div>
               </div>
-             <div>
-                 <a href="{{route('listprosale')}}"><button type="button" class="btn btn-success">Quay lại</button></a>
-                 <span><button type="submit" class="btn btn-success">Cập nhập</button></span>
+              <div class="col-md-12 center">
+                <div class="col-md-6">
+                  <a href="{{route('listprosale')}}"><button type="button" class="link-v1 rt">Quay lại</button></a>
+                </div>
+                <div class="col-md-6">
+                  <a href="#"><button type="submit" class="link-v1 rt">Cập nhật</button></a>
+                </div>
               </div>
             </div>
-            </form>
-         </div> 
-       </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</div>
 @endsection
