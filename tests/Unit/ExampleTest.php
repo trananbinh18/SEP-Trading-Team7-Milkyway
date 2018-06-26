38b46dca5e576d2cd481078b400f0b25152638f0
wingcfly <wesleynguyen1411@gmail.com>
2018-06-26 17:56:25 +0700

    Auto switch to secondary Database when running tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,16 +4,18 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic test example for test switching to secondary Database when running test!.
      *
      * @return void
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $hd = DB::table('sanpham')->first();
+        $this->assertTrue(count($hd) === 1);
     }
 }
